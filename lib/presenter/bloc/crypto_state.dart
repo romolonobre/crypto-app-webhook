d691de9a8d0b23e1cf2b3f6081381a78e35b618e
romolonobre <romoloferreira@icloud.com>
2024-08-12 22:40:33 +0100

    use switch to get states

--- lib/presenter/bloc/crypto_state.dart
+++ lib/presenter/bloc/crypto_state.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import '../../data/crypto.dart';
 
-abstract class CryptoState {}
+sealed class CryptoState {}
 
 class CryptoInitialState extends CryptoState {}
 
--- lib/presenter/crypto_view.dart
+++ lib/presenter/crypto_view.dart
@@ -31,27 +31,15 @@ class _CryptoViewState extends State<CryptoView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: BlocBuilder(
+        body: BlocBuilder<CryptoBloc, CryptoState>(
       bloc: bloc,
       builder: (context, state) {
-        if (state is CryptoLoadingState) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        if (state is CryptoErrorState) {
-          return Center(
-            child: Center(
-              child: Text(state.message),
-            ),
-          );
-        }
-        if (state is CryptoLoadedState) {
-          return Center(
-            child: Text(state.crypto.price ?? 'No info'),
-          );
-        }
-        return const Text("No data available");
+        return switch (state) {
+          CryptoInitialState _ => SizedBox.fromSize(),
+          CryptoLoadingState() => const Center(child: CircularProgressIndicator()),
+          CryptoLoadedState() => Center(child: Text(state.crypto.price ?? 'No info')),
+          CryptoErrorState() => Center(child: Text(state.message)),
+        };
       },
     ));
   }
